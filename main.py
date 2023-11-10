95a6a0ed0ca067d21d5654a4bfb48bfaa2db2308
harsha-syna <svattikuti@synamedia.com>
2023-11-10 07:21:03 +0530

    removed db code for now

--- main.py
+++ main.py
@@ -1,43 +1,4 @@
 import streamlit as st
-import psycopg2
-
-
-def pq_connect(field1, field2, field3):
-    # Connect to the PostgreSQL database
-    conn = psycopg2.connect(
-        host="your_host",
-        database="your_database",
-        user="your_user",
-        password="your_password",
-    )
-    cursor = conn.cursor()
-
-    # Create a table if it doesn't exist
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS data (
-            id SERIAL PRIMARY KEY,
-            field1 TEXT,
-            field2 TEXT,
-            field3 TEXT
-        )
-    """
-    )
-
-    # Insert data into the table
-    cursor.execute(
-        """
-        INSERT INTO data (field1, field2, field3)
-        VALUES (%s, %s, %s)
-    """,
-        (field1, field2, field3),
-    )
-    conn.commit()
-
-    # Close the database connection
-    cursor.close()
-    conn.close()
-    return True
 
 
 def main():
